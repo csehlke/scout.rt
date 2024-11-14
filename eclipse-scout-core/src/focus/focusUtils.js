3990a9989197bd07c5ab322584fdc9a3da4b775d
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2024-11-14 10:02:04 +0100

    FocusManager: fix wrong focus after dragging an element
    The global mouse down listener of the focus manager is responsible to
    allow or prevent focus gain on mouse down. If an element is draggable,
    the focus gain cannot be prevented without preventing the dragstart
    event, too.
    
    Therefore, the focus manager allows the focus gain but has to restore
    the focus afterward, so that the previously focused element will be
    focused again. This functionality broke with commit
    f318d652e5b6ec5b028f1f08fe762ead278e3951 (#381281).
    
    To fix it, the logic from FocusContext to prevent the focus gain on
    elements with tabindex=-2 has to be integrated into the global
    mouse down handler. One advantage of this approach is that such
    elements now don't get the focus at all, not even temporary, except
    if a draggable element is clicked. As explained, the focus gain must
    be allowed and may temporarily be set on an element with tabindex=-2.
    
    Also:
    - removed unnecessary HTMLElement generic of the JQuery type because
      it is the default value
    - simplified checks in isFocusableByMouse and isDraggable because
      closest also considers the current element not only the parents.
    
    397963

--- eclipse-scout-core/src/focus/focusUtils.js
+++ eclipse-scout-core/src/focus/focusUtils.js
@@ -15,28 +15,53 @@ import $ from 'jquery';
  */
 
 /**
- * @return {boolean} whether the given element is focusable by mouse.
+ * @returns {boolean} whether the given element is focusable by mouse.
  */
 export function isFocusableByMouse(element) {
-  let $element = $(element);
-  return !$element.hasClass('unfocusable') && !$element.closest('.unfocusable').length;
+  return $.ensure(element).closest('.unfocusable').length === 0;
 }
 
 /**
- * @return {boolean} whether the given element has a parent which is focusable by mouse.
+ * @returns {boolean} whether the element must not gain the focus, even if it has a tabindex. This is only true for elements with tabindex="-2".
  */
-export function containsParentFocusableByMouse(element, entryPoint) {
-  let $focusableParentElements = $(element)
-    .parentsUntil('.focus-boundary', ':focusable') // Stay inside focus boundaries (e.g. search forms should not consider parent table)
-    .not(entryPoint) /* Exclude $entryPoint as all elements are its descendants. However, the $entryPoint is only focusable to provide Portlet support. */
-    .filter(function() {
-      return isFocusableByMouse(this);
-    });
-  return ($focusableParentElements.length > 0);
+export function isFocusPrevented(element) {
+  return Number($.ensure(element).attr('tabindex')) === -2;
 }
 
 /**
- * @return {boolean} whether the given element contains content which is selectable to the user, e.g. to be copied into clipboard.
+ * @param $entryPoint the entry point of the current {@link Session}
+ * @param nativeFocusable whether to include elements that we consider to be unfocusable but would gain the focus by the browser if we did not prevent it (elements with tabindex="-2"). Default is false.
+ * @returns all parents that are focusable by mouse inside a focus boundary (marked by elements having the class .focus-boundary)
+ */
+export function getParentsFocusableByMouse(element, $entryPoint, nativeFocusable = false) {
+  return $.ensure(element)
+    .parentsUntil('.focus-boundary', nativeFocusable ? ':focusable-native' : ':focusable') // Stay inside focus boundaries (e.g. search forms should not consider parent table)
+    .not($entryPoint) // Exclude $entryPoint as all elements are its descendants. However, the $entryPoint is only focusable to provide Portlet support.
+    .filter((index, elem) => isFocusableByMouse(elem));
+}
+
+/**
+ * @param $entryPoint the entry point of the current {@link Session}
+ * @param nativeFocusable whether to include elements that we consider to be unfocusable but would gain the focus by the browser if we did not prevent it (elements with tabindex="-2"). Default is false.
+ * @returns the given element if it is focusable by mouse, or the first parent that is focusable by mouse.
+ */
+export function closestFocusableByMouse(element, $entryPoint, nativeFocusable = false) {
+  let $element = $.ensure(element);
+  if ($element.is(nativeFocusable ? ':focusable-native' : ':focusable') && isFocusableByMouse($element)) {
+    return $element;
+  }
+  return getParentsFocusableByMouse($element, $entryPoint, nativeFocusable).first();
+}
+
+/**
+ * @returns {boolean} whether the given element has a parent which is focusable by mouse.
+ */
+export function containsParentFocusableByMouse(element, $entryPoint) {
+  return getParentsFocusableByMouse(element, $entryPoint).length > 0;
+}
+
+/**
+ * @returns {boolean} whether the given element contains content which is selectable to the user, e.g. to be copied into clipboard.
  * It also returns true for disabled text-fields, because the user must be able to select and copy text from these text-fields.
  */
 export function isSelectableText(element) {
@@ -80,8 +105,7 @@ export function isSelectableText(element) {
 }
 
 /**
- * Returns true if the given HTML element is the active element in its own document, false otherwise
- * @param element
+ * @returns true if the given HTML element is the active element in its own document, false otherwise.
  */
 export function isActiveElement(element) {
   if (!element) {
@@ -98,11 +122,32 @@ export function isActiveElement(element) {
 }
 
 /**
+ * Stores the currently focused element and focuses this element again in the next animation frame if the focus changed to the entry point element.
+ * This is useful if the current task would focus the entry point element which cannot be prevented.
+ *
+ * @param $entryPoint the entry point of the current {@link Session}
+ * @param options options to be passed to the {@link HTMLElement.focus} call
+ */
+export function restoreFocusLater($entryPoint, options) {
+  // queueMicrotask does not work, it looks like the microtask will be executed before the focus change.
+  // requestAnimationFrame also prevents flickering (compared to setTimeout)
+  let doc = $entryPoint.document(true);
+  let prevFocusedElement = doc.activeElement;
+  requestAnimationFrame(() => {
+    let focusedElement = doc.activeElement;
+    // Restore previous focus if the current active element is an element we don't want to be focused (the $entryPoint or tabindex="-2")
+    if (focusedElement === $entryPoint[0] || isFocusPrevented(focusedElement)) {
+      prevFocusedElement.focus(options);
+    }
+  });
+}
+
+/**
  * Sets the focus to the given target element just before the next repaint (using requestAnimationFrame).
  * This allows other event handlers to be fired before the focus is actually changed.
  *
  * @param target the element to be focused (html or jquery element)
- * @param options options to be passed to the {@link HTMLElement#focus} call
+ * @param options options to be passed to the {@link HTMLElement.focus} call
  */
 export function focusLater(target, options) {
   let $target = $.ensure(target);
@@ -112,18 +157,25 @@ export function focusLater(target, options) {
   let doc = $target.document(true);
   let prevFocusedElement = doc.activeElement;
   requestAnimationFrame(() => {
-    // Check if the active element is the same as before. If not, someone has changed the focus
-    // in the meantime and the scheduled "focusLater" request is probably obsolete.
-    if (doc.activeElement === prevFocusedElement) {
+    // Check if the active element is the same as before. If not, someone has changed the focus in the meantime and the
+    // scheduled focusLater() request is obsolete. If the active element is considered to be unfocusable via tabindex="-2",
+    // we always change the focus to the target element. (This can happen if the global mouse down handler did not suppress
+    // the default behavior to avoid cancelling other events, e.g. 'dragstart').
+    let focusedElement = doc.activeElement;
+    if (focusedElement === prevFocusedElement || isFocusPrevented(focusedElement)) {
       $target[0].focus(options);
     }
   });
 }
 
 export default {
-  containsParentFocusableByMouse,
-  isActiveElement,
   isFocusableByMouse,
+  isFocusPrevented,
+  getParentsFocusableByMouse,
+  closestFocusableByMouse,
+  containsParentFocusableByMouse,
   isSelectableText,
+  isActiveElement,
+  restoreFocusLater,
   focusLater
 };
